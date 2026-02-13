e13dcb3115cadfd72a77b7bdfbac48d030e82ce3
Bernd Becker <bernd.becker@metoffice.gov.uk>
2026-02-13 15:03:56 +0000

    add run_tests.bash to run tests

---
+++ tests/run_tests.bash
@@ -0,0 +1,8 @@
+#/bin/bash
+python test_synthetic.py
+python test_fingerprints.py
+python test_cluster.py
+python conftest.py
+python test_gyrescore.py
+python test_canonical.py
+python test_viz_verify.py
